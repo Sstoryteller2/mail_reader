272218895566f209953d7ff3aa83ed04b4f212c0
ogiant <ogiant@student.21-school.ru>
2022-09-17 00:21:37 +0300

    improved funcs with walk

--- functions.py
+++ functions.py
@@ -1,15 +1,11 @@
 import imaplib
 import email
 from email.header import decode_header
-import webbrowser
-import os
 import traceback
 import base64
-import PyPDF2, io
 import re
 from datetime import datetime
 import config
-import sys
 from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
 import aiohttp
@@ -27,55 +23,35 @@ def connection():
         return False
 
 
-def encoded_words_to_text(encoded_words):
-    # encoded_words=encoded_words.replace("\n", "").replace("\r", "").replace("\t", "").replace(" ", "")
-    encoded_word_regex = r"=\?{1}(.+)\?{1}([B|Q])\?{1}(.+)\?{1}="
-    charset, encoding, encoded_text = re.match(
-        encoded_word_regex, encoded_words
-    ).groups()
-    if encoding == "B":
-        byte_string = base64.b64decode(encoded_text)
-    elif encoding == "Q":
-        byte_string = quopri.decodestring(encoded_text)
-    return byte_string.decode(charset)
-
-
-def get_attaches(msg):
-    attaches = list()
-    for part in msg.walk():
-        if part.get_content_disposition() == "attachment":
-            if "pdf" in part.get_content_type():
-                attaches.append(pdf_to_str(part.get_payload(decode=True)))
-    if attaches:
-        return attaches
-    else:
-        return [False]
-    
 def encode_att_names(str_pl):
-    enode_name=re.findall('\=\?.*?\?\=', str_pl)        
-    if len(enode_name) == 1:            
-        decode_name=(decode_header(enode_name[0])[0][0])            
-        decode_name=decode_name.decode("utf-8")            
-        str_pl=str_pl.replace(enode_name[0], decode_name)
-    if len(enode_name) > 1:             
-        nm=''
-        for part in enode_name:                
-            decode_name=(decode_header(part)[0][0])                        
-            decode_name=decode_name.decode("utf-8")
-            nm+=decode_name            
-        str_pl=str_pl.replace(enode_name[0], nm)    
-        for c, i in enumerate(enode_name):           
-            if c > 0:                
-                str_pl=str_pl.replace(enode_name[c], "").replace("\"",'').rstrip()
-    return str_pl  
-    
-def get_attachments(payload):    
-    attachments=list()  
-    for pl in payload:
-        if 'name' in pl["Content-Type"] and pl.get_content_disposition() == 'attachment':               
-            str_pl=pl["Content-Type"]               
-            str_pl=encode_att_names(str_pl)
-            attachments.append(str_pl)  
+    enode_name = re.findall("\=\?.*?\?\=", str_pl)
+    if len(enode_name) == 1:
+        decode_name = decode_header(enode_name[0])[0][0]
+        decode_name = decode_name.decode("utf-8")
+        str_pl = str_pl.replace(enode_name[0], decode_name)
+    if len(enode_name) > 1:
+        nm = ""
+        for part in enode_name:
+            decode_name = decode_header(part)[0][0]
+            decode_name = decode_name.decode("utf-8")
+            nm += decode_name
+        str_pl = str_pl.replace(enode_name[0], nm)
+        for c, i in enumerate(enode_name):
+            if c > 0:
+                str_pl = str_pl.replace(enode_name[c], "").replace('"', "").rstrip()
+    return str_pl
+
+
+def get_attachments(msg):
+    attachments = list()
+    for part in msg.walk():
+        if (
+            "name" in part["Content-Type"]
+            and part.get_content_disposition() == "attachment"
+        ):
+            str_pl = part["Content-Type"]
+            str_pl = encode_att_names(str_pl)
+            attachments.append(str_pl)
     return attachments
 
 
@@ -86,22 +62,9 @@ def date_parse(msg_date):
     return dt_obj
 
 
-def pdf_to_str(pdf):
-    all_doc = ""
-    try:
-        with io.BytesIO(pdf) as open_pdf_file:
-            read_pdf = PyPDF2.PdfFileReader(open_pdf_file)
-            num_pages = read_pdf.getNumPages()
-            for page in read_pdf.pages:
-                page_content = page
-                all_doc += page_content.extractText()
-            return all_doc
-    except:
-        return False
-
 async def send_message(bot_token, message, chat, rpl=None, prv=None):
     bot = Bot(token=bot_token)
-    await bot.get_session()
+    #await bot.get_session()
     obj = await bot.send_message(
         chat_id=chat,
         text=message,
@@ -112,72 +75,67 @@ async def send_message(bot_token, message, chat, rpl=None, prv=None):
     await bot._session.close()
     return obj.message_id
 
+
 def get_letter_text_from_html(body):
+    body = body.replace("<div><div>", "<div>").replace("</div></div>", "</div>")
     try:
-        soup = BeautifulSoup(body, 'html.parser')
-        paragraphs = soup.find_all('div')   
-        text=""
+        soup = BeautifulSoup(body, "html.parser")
+        paragraphs = soup.find_all("div")
+        text = ""
         for paragraph in paragraphs:
-            text += paragraph.text + "\n"    
-        return text.replace('\xa0', ' ')
+            text += paragraph.text + "\n"
+        return text.replace("\xa0", " ")
     except (Exception) as exp:
-        print('text ftom html err ', exp)
+        print("text ftom html err ", exp)
         return False
 
-def decode_text(payload):    
-    if payload.get_content_subtype() == 'plain':            
+
+def decode_text(payload):
+    if payload.get_content_subtype() == "plain":
         letter_text = (
-            (base64.b64decode(payload.get_payload()).decode())
-            .lstrip()
-            .rstrip()
-        )        
-        letter_text = letter_text.replace("<","").replace(">","").replace("\xa0","")
+            (base64.b64decode(payload.get_payload()).decode()).lstrip().rstrip()
+        )
+        letter_text = letter_text.replace("<", "").replace(">", "").replace("\xa0", "")
         return letter_text
-    if payload.get_content_subtype() == 'html':
+    if payload.get_content_subtype() == "html":
         try:
             letter_text = (
-                    (base64.b64decode(payload.get_payload()).decode())
-                    .lstrip()
-                    .rstrip()
-                    )        
-            letter_text=get_letter_text_from_html(letter_text)
+                (base64.b64decode(payload.get_payload()).decode()).lstrip().rstrip()
+            )
+            letter_text = get_letter_text_from_html(letter_text)
         except:
             letter_text = get_letter_text_from_html(payload.get_payload())
         return letter_text
     else:
         return False
-    
-def get_text_from_multipart(payload):
-    for pl in payload:         
-        count=0
-        if pl.is_multipart():            
-            level = pl.get_payload()
-            for part in level:
-                if part.get_content_maintype() == 'text' and count == 0:         
-                    letter_text = decode_text(part)
-                    count += 1                                        
-        else:        
-            if pl.get_content_maintype() == 'text' and count == 0:           
-                letter_text = decode_text(pl)                     
-        return letter_text
+
+
+def get_text_from_multipart(msg):
+    for part in msg.walk():
+        count = 0
+        if part.get_content_maintype() == "text" and count == 0:
+            letter_text = decode_text(part)
+            count += 1
+            return letter_text
+
 
 def post_construct(msg_subj, msg_from, msg_email, letter_text, attachments):
-    att_txt=""
+    att_txt = ""
     for atts in attachments:
-        att_txt+=(atts.strip()+"\n")
+        att_txt += atts.strip() + "\n"
     txt = ""
     txt += (
-        "<b>"
+        "\U0001F4E8 <b>"
         + str(msg_subj)
         + "</b>"
-        + "\n\n"
+        + "\n\n<pre>"
         + str(msg_from)
         + "\n"
         + msg_email
-        + "\n"
+        + "</pre>\n\n"
         + letter_text
         + "\n\n"
-        + "<i>вложения: </i>"
+        + "\U0001F4CE<i> вложения: </i>"
         + str(len(attachments))
         + "\n\n"
         + att_txt
--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from email.header import decode_header
 import base64
 import sys
 import time
-import asyncio   
+import asyncio
 import traceback
+
 
 def main():
     imap = functions.connection()
@@ -17,20 +18,19 @@ def main():
         sys.exit()
 
     status, messages = imap.select("INBOX")  # папка входящие
-    res, unseen_msg = imap.uid('search', "UNSEEN", "ALL")
+    res, unseen_msg = imap.uid("search", "UNSEEN", "ALL")
     unseen_msg = unseen_msg[0].decode("utf-8").split(" ")
 
     if unseen_msg[0]:
         for letter in unseen_msg:
-            attachments=[]
-            res, msg = imap.uid('fetch', letter, '(RFC822)')            
+            attachments = []
+            res, msg = imap.uid("fetch", letter, "(RFC822)")
             if res == "OK":
-                #получаем всю информацию о письме
                 msg = email.message_from_bytes(msg[0][1])
                 msg_date = email.utils.parsedate_tz(msg["Date"])
                 msg_date = functions.date_parse(msg_date)
-                
-                msg_from = decode_header(msg["From"])                
+
+                msg_from = decode_header(msg["From"])
                 try:
                     if type(msg_from[0][0]) == type("str"):
                         msg_from = str(msg_from[0][0]).strip("<>").replace("<", "")
@@ -38,29 +38,29 @@ def main():
                         msg_from = decode_header(msg["From"])[0][0].decode()
                     msg_subj = decode_header(msg["Subject"])[0][0].decode()
                 except:
-                    msg_from = decode_header(msg["From"])[0][0].decode()#.strip("<>").rstrip(">")
-                    msg_from = msg_from#.replace("<", "")
-                    msg_subj = (msg["Subject"])
+                    msg_from = decode_header(msg["From"])
+                    msg_from = msg_from[0][0].decode()
+                    print(msg_from)
+                    msg_subj = msg["Subject"]
                     if msg_subj != None:
                         msg_subj = decode_header(msg["Subject"])[0][0]
-                        
+
                 msg_id = msg["Message-ID"].lstrip("<").rstrip(">")
                 msg_email = msg["Return-path"].lstrip("<").rstrip(">")
-                
-                payload = msg.get_payload()
-                
+
                 if msg.is_multipart():
-                    letter_text=functions.get_text_from_multipart(payload)
-                    attachments = functions.get_attachments(payload)                    
-                else:                    
-                    letter=base64.b64decode(payload).decode()
-                    letter_text=functions.get_letter_text_from_html(letter)
+                    letter_text = functions.get_text_from_multipart(msg)
+                    attachments = functions.get_attachments(msg)
+                else:
+                    payload = msg.get_payload()
+                    letter = base64.b64decode(payload).decode()
+                    letter_text = functions.get_letter_text_from_html(letter)
                 post_text = functions.post_construct(
-                        msg_subj, msg_from, msg_email, letter_text, attachments
-                        )                
+                    msg_subj, msg_from, msg_email, letter_text, attachments
+                )
                 if len(post_text) > 4000:
                     post_text = post_text[:4000]
-                    
+
                 loop = asyncio.get_event_loop()
                 loop.run_until_complete(
                     functions.send_message(config.bot_key, post_text, config.chat_id)
@@ -74,10 +74,10 @@ def main():
 if __name__ == "__main__":
     try:
         main()
-    except (Exception) as exp:        
-        text=str("ошибка: " + str(exp))
+    except (Exception) as exp:
+        text = str("ошибка: " + str(exp))
         print(traceback.format_exc())
         loop = asyncio.get_event_loop()
         loop.run_until_complete(
-                functions.send_message(config.bot_key, text, config.chat_id)
-                )
+            functions.send_message(config.bot_key, text, config.chat_id)
+        )

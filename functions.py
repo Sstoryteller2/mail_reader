c17b759bc8aefe0f78b1ea5cb9f0d98fdb787ed9
ogiant <ogiant@student.21-school.ru>
2022-11-09 23:20:54 +0300

    encoding added

--- functions.py
+++ functions.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
 import aiohttp
 import asyncio
+import quopri
 
-ENCODING=config.encoding
 
 def connection():
     mail_pass = config.mail_pass
@@ -28,14 +28,16 @@ def connection():
 def encode_att_names(str_pl):
     enode_name = re.findall("\=\?.*?\?\=", str_pl)
     if len(enode_name) == 1:
+        encoding = decode_header(enode_name[0])[0][1]
         decode_name = decode_header(enode_name[0])[0][0]
-        decode_name = decode_name.decode(ENCODING)
+        decode_name = decode_name.decode(encoding)
         str_pl = str_pl.replace(enode_name[0], decode_name)
     if len(enode_name) > 1:
         nm = ""
         for part in enode_name:
+            encoding = decode_header(part)[0][1]
             decode_name = decode_header(part)[0][0]
-            decode_name = decode_name.decode(ENCODING)
+            decode_name = decode_name.decode(encoding)
             nm += decode_name
         str_pl = str_pl.replace(enode_name[0], nm)
         for c, i in enumerate(enode_name):
@@ -48,7 +50,8 @@ def get_attachments(msg):
     attachments = list()
     for part in msg.walk():
         if (
-            "name" in part["Content-Type"]
+            part["Content-Type"]
+            and "name" in part["Content-Type"]
             and part.get_content_disposition() == "attachment"
         ):
             str_pl = part["Content-Type"]
@@ -58,17 +61,21 @@ def get_attachments(msg):
 
 
 def date_parse(msg_date):
-    dt_obj = "".join(str(msg_date[:6]))
-    dt_obj = dt_obj.strip("'(),")
-    dt_obj = datetime.strptime(dt_obj, "%Y, %m, %d, %H, %M, %S")
-    return dt_obj
+    if not msg_date:
+        return datetime.now()
+    else:
+        dt_obj = "".join(str(msg_date[:6]))
+        dt_obj = dt_obj.strip("'(),")
+        dt_obj = datetime.strptime(dt_obj, "%Y, %m, %d, %H, %M, %S")
+        return dt_obj
 
 
 def from_subj_decode(msg_from_subj):
     if msg_from_subj:
+        encoding = decode_header(msg_from_subj)[0][1]
         msg_from_subj = decode_header(msg_from_subj)[0][0]
         if isinstance(msg_from_subj, bytes):
-            msg_from_subj = msg_from_subj.decode(ENCODING)
+            msg_from_subj = msg_from_subj.decode(encoding)
         if isinstance(msg_from_subj, str):
             pass
         msg_from_subj = str(msg_from_subj).strip("<>").replace("<", "")
@@ -120,8 +127,12 @@ def get_letter_text_from_html(body):
 def letter_type(part):
     if part["Content-Transfer-Encoding"] in (None, "7bit", "8bit", "binary"):
         return part.get_payload()
-    if part["Content-Transfer-Encoding"] == "base64":
-        return base64.b64decode(part.get_payload()).decode(ENCODING)
+    elif part["Content-Transfer-Encoding"] == "base64":
+        encoding = part.get_content_charset()
+        return base64.b64decode(part.get_payload()).decode(encoding)
+    elif part["Content-Transfer-Encoding"] == "quoted-printable":
+        encoding = part.get_content_charset()
+        return quopri.decodestring(part.get_payload()).decode(encoding)
     else:  # all possible types: quoted-printable, base64, 7bit, 8bit, and binary
         return part.get_payload()
 
@@ -135,7 +146,7 @@ def get_letter_text(msg):
                 if part.get_content_subtype() == "html":
                     letter_text = get_letter_text_from_html(extract_part)
                 else:
-                    letter_text = extract_part
+                    letter_text = extract_part.rstrip().lstrip()
                 count += 1
                 return (
                     letter_text.replace("<", "").replace(">", "").replace("\xa0", " ")
@@ -166,24 +177,23 @@ def send_attach(msg, msg_subj, repl):
 
 
 def post_construct(msg_subj, msg_from, msg_email, letter_text, attachments):
-    att_txt = ""
-    for atts in attachments:
-        att_txt += atts.strip() + "\n"
-    txt = ""
-    txt += (
-        "\U0001F4E8 <b>"
-        + str(msg_subj)
-        + "</b>"
-        + "\n\n<pre>"
-        + str(msg_from)
-        + "\n"
-        + msg_email
-        + "</pre>\n\n"
-        + letter_text
-        + "\n\n"
-        + "\U0001F4CE<i> вложения: </i>"
-        + str(len(attachments))
-        + "\n\n"
-        + att_txt
-    )
+    att_txt = "".join(attachments)
+    postparts = [
+        "\U0001F4E8 <b>",
+        str(msg_subj),
+        "</b>\n\n",
+        "<pre>",
+        str(msg_from),
+        "\n",
+        msg_email,
+        "</pre>\n\n",
+        letter_text,
+        "\n\n",
+        "\U0001F4CE<i> вложения: </i>",
+        str(len(attachments)),
+        "\n\n",
+        att_txt,
+    ]
+    txt = "".join(postparts)
+
     return txt
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ import time
 import asyncio
 import traceback
 
-ENCODING=config.encoding
+
+ENCODING = config.encoding
+
 
 def main():
     imap = functions.connection()
@@ -28,16 +30,23 @@ def main():
             res, msg = imap.uid("fetch", letter, "(RFC822)")
             if res == "OK":
                 msg = email.message_from_bytes(msg[0][1])
-
                 msg_date = functions.date_parse(email.utils.parsedate_tz(msg["Date"]))
                 msg_from = functions.from_subj_decode(msg["From"])
                 msg_subj = functions.from_subj_decode(msg["Subject"])
-                msg_id = msg["Message-ID"].lstrip("<").rstrip(">")
-                msg_email = msg["Return-path"].lstrip("<").rstrip(">")
+                if msg["Message-ID"]:
+                    msg_id = msg["Message-ID"].lstrip("<").rstrip(">")
+                else:
+                    msg_id = msg["Received"]
+                if msg["Return-path"]:
+                    msg_email = msg["Return-path"].lstrip("<").rstrip(">")
+                else:
+                    msg_email = msg_from
+
                 if not msg_email:
+                    encoding = decode_header(msg["From"])[0][1]  # не проверено
                     msg_email = (
                         decode_header(msg["From"])[1][0]
-                        .decode(ENCODING)
+                        .decode(encoding)
                         .replace("<", "")
                         .replace(">", "")
                         .replace(" ", "")
@@ -51,10 +60,10 @@ def main():
                 )
                 if len(post_text) > 4000:
                     post_text = post_text[:4000]
-                
+
                 loop = asyncio.new_event_loop()
                 asyncio.set_event_loop(loop)
-                
+
                 reply_id = loop.run_until_complete(
                     functions.send_message(config.bot_key, post_text, config.chat_id)
                 )
